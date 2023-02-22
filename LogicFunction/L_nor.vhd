b6e21f3ef3b6debbdac02b7e4ed554e4ac454974
thallesmcs <thalles_mikael@hotmail.com>
2023-02-22 18:03:59 -0300

    Add L_nor file

---
+++ LogicFunction/L_nor.vhd
@@ -0,0 +1,17 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity L_nor is
+  port(
+		a,b: in std_logic;
+		x: out std_logic
+		);
+end L_nand;
+
+architecture comportamento of L_nor is
+
+begin
+
+x <= a nor b;
+
+end comportamento;
